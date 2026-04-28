60057b929dce337726b755b49f840a613b0fe10a
Manoah Kinnaert <manoah.k@icloud.com>
2026-04-28 13:29:39 +0200

    naive topdown

--- Collatz/__main__.py
+++ Collatz/__main__.py
@@ -25,6 +25,17 @@ def run_collatz_recursive(n: int):
         if num == 1: break 
     return i - 1
 
+def run_collatz_top_down(n: int):
+    i = 0
+    nums = [n]
+    while True:
+        n = f(n)
+        i += 1
+        nums.append(n)
+        if n == 1: break 
+    print(nums)
+    return i
+
 def determine_collatz_recursive(max_n: int):
     N = range(1, max_n + 1)
     steps = []
@@ -36,6 +47,27 @@ def determine_collatz_recursive(max_n: int):
     print("[DEBUG]: Max N =", len(steps))
     return steps 
 
+def determine_collatz_top_down(max_n: int):
+    N = range(1, max_n + 1)
+    steps = []
+    for n in N:
+        step = run_collatz_top_down(n)
+        steps.append(step)
+
+    print("[DEBUG]: Steps in function of n:", steps)
+    print("[DEBUG]: Max N =", len(steps))
+    return steps 
+
+def run_collatz_exp_topdown(max_n: int):
+    times = []
+    for n in range(1, max_n + 1):
+        t1 = datetime.now()
+        determine_collatz_top_down(n)
+        t2 = datetime.now()
+        delta_t = t2 - t1 
+        times.append(delta_t.total_seconds() * 1000)
+    return times 
+
 def run_collatz_exp_recursive(max_n: int):
     times = []
     for n in range(1, max_n + 1):
@@ -46,9 +78,13 @@ def run_collatz_exp_recursive(max_n: int):
         times.append(delta_t.total_seconds() * 1000)
     return times 
 
-if __name__ == "__main__":
+def main():
     max_n = os.getenv("N")
     if max_n is None: max_n = 100
-    times = run_collatz_exp_recursive(max_n)
-    print("Time required per n in milliseconds:", times)
-    plot(times)
+    times_recursive = run_collatz_exp_recursive(max_n)
+    times_topdown = run_collatz_exp_topdown(max_n)
+    print("Time required per n in milliseconds for recursive determination:", times_recursive)
+    plot(times_recursive, times_topdown)
+
+if __name__ == "__main__":
+    main()
--- Collatz/plot.py
+++ Collatz/plot.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt 
 
-def plot(recursive: list):
-    fig, ax = plt.subplots(1, 1)
-    ax.plot(range(1, len(recursive) + 1), recursive)
+def plot(recursive: list, top_down: list):
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.plot(range(1, len(recursive) + 1), recursive, label="time recursive")
+    ax2.plot(range(1, len(top_down) + 1), top_down, label="time top down")
 
     plt.show()
