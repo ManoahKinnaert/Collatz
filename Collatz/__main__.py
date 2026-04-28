18abe84abe6e4a5983294d0458af0d1a0023ad33
Manoah Kinnaert <manoah.k@icloud.com>
2026-04-28 13:06:18 +0200

    more collatz recursive stuff

--- Collatz/__main__.py
+++ Collatz/__main__.py
@@ -1,3 +1,7 @@
+import sys 
+
+sys.setrecursionlimit(10000)
+
 def f(n: int):
     if not n % 2: return n // 2
     else: return 3 * n + 1
@@ -8,14 +12,22 @@ def collatz_recursive(i: int, n: int):
     else: print("Invalid")
 
 
-if __name__ == "__main__":
-    import os
-    n = int(os.getenv("N"))
-    print(n)
+def run_collatz_recursive(n: int):
     i = 0
     while True:
-        num = collatz_recursive(i, 12)
+        num = collatz_recursive(i, n)
         i += 1
-        print(num)
         if num == 1: break 
-    print("Num of steps:", i - 1)
+    return i - 1
+
+def run_collatz_recursive_exp(max_n: int):
+    N = range(1, max_n + 1)
+    steps = []
+    for n in N:
+        step = run_collatz_recursive(n)
+        steps.append(step)
+
+    print("Steps in function of n:", steps)
+
+if __name__ == "__main__":
+    run_collatz_recursive_exp(max_n=15)
