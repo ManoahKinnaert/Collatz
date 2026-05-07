8c0f3177ae2af80b02d6c083119013eba3e7ddcf
Manoah Kinnaert <manoah.k@icloud.com>
2026-05-07 11:19:31 +0200

    Simple

--- Collatz/__main__.py
+++ Collatz/__main__.py
@@ -2,15 +2,12 @@ def f(n :int):
     if not n % 2: return n // 2
     else: return 3 * n + 1
 
-def collatz_recursive(i: int, n: int):
-    if i == 0: return n 
-    elif i > 0: return f(collatz_recursive(i - 1, n))
-
-def count_recursive_steps():
-    pass  
+def collatz_recursive(n :int):
+    if n == 1: return 0
+    return 1 + collatz_recursive(f(n))
 
 def main():
-    pass 
+    print(collatz_recursive(12))
 
 if __name__ == "__main__":
-    pass 
+    main() 
